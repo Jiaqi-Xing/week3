a9936f0552c80df54309c4682b5a9d6f92b4b874
Jiaqi-Xing <ariel.xing@mail.utoronto.ca>
2024-09-19 20:25:25 -0400

    Add files via upload

---
+++ starter_folder-main (1)/starter_folder-main/scripts/03-test_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Tests... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Test data ####
+data <- read_csv("~/sta304/starter_folder-main/data/raw_data/simulated_data.csv")
+
+## Test for negative numbers
+
+## Test for NAs
